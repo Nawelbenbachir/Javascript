72812c3254e05a10561cc41097c36065862f8a1e
Nawel Benbachir <benbachirnawel@gmail.com>
2025-01-20 16:23:54 +0100

    mois2

--- mois.js
+++ mois.js
@@ -2,7 +2,10 @@ function mois(i){
 
     //Retourner la chaîne de caractère correspondant au mois numérique demandé
     //En utilisant un tableau
-    tableau=["","Janvier","Février","Mars","Avril","Mai","Juin","Juillet","Août","Septembre","Octobre","Novembre","Décembre"];
-    return (tableau[i]);
+    tableau=["Janvier","Février","Mars","Avril","Mai","Juin","Juillet","Août","Septembre","Octobre","Novembre","Décembre"];
+    if (i<=12){
+        return (tableau[i-1]);
+    }
+        return "";
 }
-console.log(mois(6));
+console.log(mois(13));
